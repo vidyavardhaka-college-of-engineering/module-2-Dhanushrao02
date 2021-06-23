c4996a77c14981a925fe31fbbebf525e6c2c3379
Dhanushrao02 <84696628+Dhanushrao02@users.noreply.github.com>
2021-06-23 19:12:34 +0530

    Update Program-2.c

--- Program-2.c
+++ Program-2.c
@@ -4,7 +4,7 @@
 
 
 #include<stdio.h>
-int main()
+void main()
 {
 	int i=1,n;
 	printf("Enter upper limit : ");
@@ -12,5 +12,5 @@ int main()
 	printf("The natural numbers are ");
 	for(i=1;i<=n;i++)
 		printf("%d ",i);
-   return 0;//Fill the missing code here
+	//Fill the missing code here
  }
